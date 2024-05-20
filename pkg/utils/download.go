3bad8f4d8c45be3de8324c4c51f016ac0cb8cc01
KotonBads <kotonbads@duck.com>
2024-05-20 22:05:02 +0800

    feat: file downloader util

---
+++ pkg/utils/download.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func DownloadFile(path string, url string, timeout time.Duration) error {
+	err := os.MkdirAll(path, os.ModePerm)
+	if err != nil {
+		return fmt.Errorf("error creating dirs: %s", err)
+	}
+
+	out, err := os.Create(path)
+	if err != nil {
+		return fmt.Errorf("error creating [%s]: %s", path, err)
+	}
+	defer out.Close()
+
+	client := http.Client{
+		Timeout: timeout,
+	}
+
+	resp, err := client.Get(url)
+	if err != nil {
+		return fmt.Errorf("error in request: %s", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("bad status: %s", resp.Status)
+	}
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		return fmt.Errorf("error copying bytes: %s", err)
+	}
+
+	return nil
+}
