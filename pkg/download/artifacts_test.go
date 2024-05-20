133bfc1cc0f6091a1ec39258dce6cb2a6220b55c
KotonBads <kotonbads@duck.com>
2024-05-21 01:28:09 +0800

    chore: test artifact downloading

---
+++ pkg/download/artifacts_test.go
@@ -0,0 +1,24 @@
+package download
+
+import (
+	"testing"
+
+	"github.com/KotonBads/MultiLunar/pkg/api"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDownloadArtifact(t *testing.T) {
+	body := api.LaunchBody{
+		OS: "linux",
+		Version: "1.8.9",
+		Module: "forge",
+		Arch: "x64",
+	}
+
+	meta, err := body.FetchLaunchMeta()
+	assert.Equal(t, nil, err)
+
+	status, err := DownloadArtifacts(meta, "/home/koton-bads/Documents/Go/MultiLunar/temp/")
+	assert.Equal(t, nil, err)
+	assert.Equal(t, 0, status)
+}
