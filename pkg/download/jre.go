92b7ea7140b8d484554acf5d8d7c19b148ed124b
KotonBads <kotonbads@duck.com>
2024-05-21 03:09:41 +0800

    feat: jre downloader

--- pkg/download/jre.go
+++ pkg/download/jre.go
@@ -1,1 +1,34 @@
 package download
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/KotonBads/MultiLunar/pkg/api"
+	"github.com/KotonBads/MultiLunar/pkg/utils"
+	"github.com/c4milo/unpackit"
+)
+
+func DownloadJRE(data api.LaunchMeta, path string) error {
+	if !data.Success {
+		return fmt.Errorf("error: data.Success is false")
+	}
+
+	out := filepath.Join(path, data.Jre.ExecutablePathInArchive[0]+data.Jre.Download.Extension)
+
+	err := utils.DownloadFile(out, data.Jre.Download.URL, time.Second*120)
+	if err != nil {
+		return fmt.Errorf("error downloading jre: %s", err)
+	}
+
+	file, err := os.Open(out)
+	if err != nil {
+		return fmt.Errorf("error opening file: %s", err)
+	}
+	defer file.Close()
+	unpackit.Unpack(file, path)
+
+	return nil
+}
