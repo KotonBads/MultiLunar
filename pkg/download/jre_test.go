d6ce97bde6654f0b5481fe4bfb80ad1c2589fea6
KotonBads <kotonbads@duck.com>
2024-05-21 03:09:54 +0800

    chore: test jre downloader

---
+++ pkg/download/jre_test.go
@@ -0,0 +1,23 @@
+package download
+
+import (
+	"testing"
+
+	"github.com/KotonBads/MultiLunar/pkg/api"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestJRE(t *testing.T) {
+	body := api.LaunchBody{
+		OS:      "linux",
+		Version: "1.8.9",
+		Module:  "forge",
+		Arch:    "x64",
+	}
+
+	meta, err := body.FetchLaunchMeta()
+	assert.Equal(t, nil, err)
+
+	err = DownloadJRE(meta, "../../temp/")
+	assert.Equal(t, nil, err)
+}
