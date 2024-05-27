1e45ca04301ce63605e6b0595ae4e27a1da16283
KotonBads <kotonbads@duck.com>
2024-05-27 18:10:33 +0800

    feat: natives unpacking

--- pkg/download/natives.go
+++ pkg/download/natives.go
@@ -1,3 +1,29 @@
 package download
 
-// use github.com/c4milo/unpackit to extract natives
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/KotonBads/MultiLunar/pkg/api"
+	"github.com/KotonBads/MultiLunar/pkg/utils"
+	"github.com/c4milo/unpackit"
+)
+
+func UnpackNatives(data api.LaunchMeta, path string) error {
+	if !data.Success {
+		return fmt.Errorf("error: data.Success is false")
+	}
+
+	dirPath := filepath.Dir(path)
+	out := utils.CategorizeArtifacts(data, dirPath)["NATIVES"][0]
+
+	file, err := os.Open(out)
+	if err != nil {
+		return fmt.Errorf("error opening file: %s", err)
+	}
+	defer file.Close()
+	unpackit.Unpack(file, path)
+
+	return nil
+}
