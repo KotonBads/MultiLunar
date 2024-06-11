fc5c9cef5ff9d8dcb6e2d44672a226d211c1e980
KotonBads <kotonbads@duck.com>
2024-06-11 23:53:40 +0800

    feat: separate input and output paths

--- pkg/download/natives.go
+++ pkg/download/natives.go
@@ -3,20 +3,18 @@ package download
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 
 	"github.com/KotonBads/MultiLunar/pkg/api"
 	"github.com/KotonBads/MultiLunar/pkg/utils"
 	"github.com/c4milo/unpackit"
 )
 
-func UnpackNatives(data api.LaunchMeta, path string) error {
+func UnpackNatives(data api.LaunchMeta, native string, path string) error {
 	if !data.Success {
 		return fmt.Errorf("error: data.Success is false")
 	}
 
-	dirPath := filepath.Dir(path)
-	out := utils.CategorizeArtifacts(data, dirPath)["NATIVES"][0]
+	out := utils.CategorizeArtifacts(data, native)["NATIVES"][0]
 
 	file, err := os.Open(out)
 	if err != nil {
